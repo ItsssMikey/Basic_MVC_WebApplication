fd5719f7ea9a96470a8e74717ce4a9ae66ba01c2
dennigm <demat5113@oslomet.no>
2025-10-26 02:49:30 +0100

    Kommentar til designTimeFRactory

--- QuizApp.Data/DesignTimeFactory.cs
+++ QuizApp.Data/DesignTimeFactory.cs
@@ -2,7 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 
 namespace QuizApp.Data;
-
+//dette er for EF core når den lager migrasjonene sine
+//kjøres ikke som en del av selve applikasjonen
 public class DesignTimeFactory : IDesignTimeDbContextFactory<AppDbContext>
 {
     public AppDbContext CreateDbContext(string[] args)
